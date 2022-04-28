69ebe1922ebec4b859067db9a4f37f5867af7e62
Rosie <Rosie@daangn.com>
2022-04-28 21:52:03 +0900

    chore: rename modules

--- rendezvous-rest/build.gradle.kts
+++ rendezvous-rest/build.gradle.kts
@@ -1,9 +1,8 @@
 dependencies {
-    implementation(project(":join-core"))
-    implementation(project(":join-data"))
+    implementation(project(":rendezvous-core"))
+    implementation(project(":rendezvous-data"))
 
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-
 }
