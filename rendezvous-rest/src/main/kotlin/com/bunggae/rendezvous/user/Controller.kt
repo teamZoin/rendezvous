e4e91d0dfb94386beeb1eacea29573e1d5d7a076
Kayoung <rkdkdudjd@gmail.com>
2022-05-01 23:51:22 +0900

    feat: 인증 메일 발송 API

--- rendezvous-rest/src/main/kotlin/com/bunggae/rendezvous/user/Controller.kt
+++ rendezvous-rest/src/main/kotlin/com/bunggae/rendezvous/user/Controller.kt
@@ -4,9 +4,11 @@ import com.bunggae.rendezvous.common.Response
 import com.bunggae.rendezvous.user.application.usecase.CheckAlreadyExistingEmailUseCase
 import com.bunggae.rendezvous.user.application.usecase.CheckAlreadyExistingServiceIdUseCase
 import com.bunggae.rendezvous.user.application.usecase.CreateUserUseCase
+import com.bunggae.rendezvous.user.application.usecase.SendVerificationEmailUseCase
 import com.bunggae.rendezvous.user.dto.CheckExistingServiceIdReqDto
 import com.bunggae.rendezvous.user.dto.CheckExitingEmailReqDto
 import com.bunggae.rendezvous.user.dto.UserSignUpReqDto
+import com.bunggae.rendezvous.user.dto.VerifyEmailReqDto
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -19,6 +21,7 @@ class Controller(
     private val createUserUseCase: CreateUserUseCase,
     private val checkAlreadyExistingEmailUseCase: CheckAlreadyExistingEmailUseCase,
     private val checkAlreadyExistingServiceIdUseCase: CheckAlreadyExistingServiceIdUseCase,
+    private val sendVerificationEmailUseCase: SendVerificationEmailUseCase,
 ) {
     @PostMapping("/sign-up")
     fun signUp(
@@ -61,6 +64,17 @@ class Controller(
         return Response(
             status = HttpStatus.OK.value(),
             message = if (serviceIdAlreadyExists) "이미 존재하는 아이디입니다." else "사용 가능한 아이디입니다.",
+        )
+    }
+
+    @PostMapping("/verification")
+    fun sendEmailVerification(
+        @RequestBody req: VerifyEmailReqDto
+    ): Response<Unit> {
+        sendVerificationEmailUseCase.execute(SendVerificationEmailUseCase.Command(req.email))
+        return Response(
+            status = HttpStatus.OK.value(),
+            message = "인증 이메일 전송에 성공했습니다."
         )
     }
 }
