4d7b55a0dabd1be98f7574c17f2ce8d67ad5d18e
Kayoung <rkdkdudjd@gmail.com>
2022-05-08 19:04:44 +0900

    feat: 이메일 전송 후 코드 정보 저장하기

--- rendezvous-core/src/main/kotlin/com/bunggae/rendezvous/user/application/usecase/SendVerificationEmailUseCase.kt
+++ rendezvous-core/src/main/kotlin/com/bunggae/rendezvous/user/application/usecase/SendVerificationEmailUseCase.kt
@@ -1,11 +1,14 @@
 package com.bunggae.rendezvous.user.application.usecase
 
+import com.bunggae.rendezvous.user.application.aggregate.EmailAuthAggregate
 import com.bunggae.rendezvous.user.application.infra.MailService
+import com.bunggae.rendezvous.user.domain.EmailAuth
 import javax.inject.Named
 
 @Named
 class SendVerificationEmailUseCase(
     private val mailService: MailService,
+    private val emailAuthAggregate: EmailAuthAggregate,
 ) {
     data class Command(
         val email: String
@@ -14,7 +17,14 @@ class SendVerificationEmailUseCase(
     fun execute(command: Command) {
         val (targetEmail) = command
         val randomCode = genVerificationCode()
+
         mailService.sendVerificationEmail(targetEmail, randomCode)
+
+        val newEmailAuth = EmailAuth(
+            email = targetEmail,
+            code = randomCode,
+        )
+        emailAuthAggregate.save(newEmailAuth)
     }
 
     private fun genVerificationCode(): String {
