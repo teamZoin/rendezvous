bb4dde79e2c656c4fe72f7a39d8515135425c43d
Kayoung <rkdkdudjd@gmail.com>
2022-05-11 12:12:21 +0900

    chore: make domain field setter private

--- rendezvous-core/src/main/kotlin/com/bunggae/rendezvous/user/domain/User.kt
+++ rendezvous-core/src/main/kotlin/com/bunggae/rendezvous/user/domain/User.kt
@@ -3,20 +3,53 @@ package com.bunggae.rendezvous.user.domain
 import java.time.LocalDateTime
 
 class User(
-    var id: Long? = null,
-    var email: String,
-    var hashedPassword: ByteArray,
-    var salt: ByteArray,
-    var serviceId: String,
-    var userName: String,
-    var profileImgUrl: String? = null,
-    var createdAt: LocalDateTime = LocalDateTime.now(),
-    var updatedAt: LocalDateTime = LocalDateTime.now(),
-    var deletedAt: LocalDateTime? = null,
+    id: Long? = null,
+    email: String,
+    hashedPassword: ByteArray,
+    salt: ByteArray,
+    serviceId: String,
+    userName: String,
+    profileImgUrl: String? = null,
+    createdAt: LocalDateTime = LocalDateTime.now(),
+    updatedAt: LocalDateTime = LocalDateTime.now(),
+    deletedAt: LocalDateTime? = null,
+    agreedToPushNotification: Boolean = true,
 ) {
-    var agreedToPushNotification: Boolean = true
+    var id: Long? = id
+    var email: String = email
+        private set
+    var hashedPassword: ByteArray = hashedPassword
+        private set
+    var salt: ByteArray = salt
+        private set
+    var serviceId: String = serviceId
+        private set
+    var userName: String = userName
+        private set
+    var profileImgUrl: String? = profileImgUrl
+        private set
+    var createdAt: LocalDateTime = createdAt
+        private set
+    var updatedAt: LocalDateTime = updatedAt
+        private set
+    var deletedAt: LocalDateTime? = deletedAt
+        private set
+    var agreedToPushNotification: Boolean = agreedToPushNotification
+        private set
 
     fun isDeleted(): Boolean = deletedAt != null
+
+    fun changeProfileImg(newUrl: String) {
+        profileImgUrl = newUrl
+    }
+
+    fun agreeToGetNotification() {
+        agreedToPushNotification = true
+    }
+
+    fun disagreeToGetNotification() {
+        agreedToPushNotification = false
+    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
