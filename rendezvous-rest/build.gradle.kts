a23a15b499c660d7845ea0d0a389737e5680ce0f
Kayoung <rkdkdudjd@gmail.com>
2022-05-12 22:29:01 +0900

    feat: argument resolver for authorization token

--- rendezvous-rest/build.gradle.kts
+++ rendezvous-rest/build.gradle.kts
@@ -13,5 +13,10 @@ dependencies {
     implementation("com.sun.mail:jakarta.mail:_")
     implementation("com.sun.activation:jakarta.activation:_")
 
+    // jwt
+    implementation("io.jsonwebtoken:jjwt-api:_")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:_")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:_")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
