eead4271954afbb96c5fb2d4f5c7b48befa93f2b
Kayoung <rkdkdudjd@gmail.com>
2022-05-18 12:19:14 +0900

    feat: user domain methods

--- rendezvous-core/src/main/kotlin/com/zoin/rendezvous/domain/user/User.kt
+++ rendezvous-core/src/main/kotlin/com/zoin/rendezvous/domain/user/User.kt
@@ -2,12 +2,17 @@ package com.zoin.rendezvous.domain.user
 
 import com.zoin.rendezvous.base.JpaBaseEntity
 import com.zoin.rendezvous.base.SoftDeletable
+import com.zoin.rendezvous.domain.rendezvous.Rendezvous
+import com.zoin.rendezvous.domain.rendezvous.repository.RendezvousRepository
 import org.hibernate.annotations.SQLDelete
 import java.time.LocalDateTime
 import javax.persistence.Entity
+import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.ManyToMany
+import javax.persistence.OneToMany
 import javax.persistence.Table
 
 @Entity
@@ -30,24 +35,50 @@ class User(
     updatedAt: LocalDateTime = LocalDateTime.now(),
     override var deletedAt: LocalDateTime? = null,
 ) : JpaBaseEntity(createdAt, updatedAt), SoftDeletable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = id
 
     var email: String = email
         private set
+
     var hashedPassword: ByteArray = hashedPassword
         private set
+
     var salt: ByteArray = salt
         private set
+
     var serviceId: String = serviceId
         private set
+
     var userName: String = userName
         private set
+
     var profileImgUrl: String? = profileImgUrl
         private set
+
     var agreedToPushNotifications: Boolean = agreedToPushNotifications
         private set
+
+    @OneToMany(mappedBy = "creator", fetch = FetchType.LAZY)
+    var madeRendezvous: List<Rendezvous> = listOf()
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    var belongsToRendezvous: List<Rendezvous> = listOf()
+
+    fun participateRendezvous(rendezvous: Rendezvous) {
+        if (rendezvous.isClosed()) throw IllegalAccessException("이미 마감된 번개입니다.")
+        rendezvous.addNewParticipant(this)
+    }
+
+    fun deleteRendezvous(rendezvousRepository: RendezvousRepository, rendezvous: Rendezvous) {
+        val mutableRendezvousList = madeRendezvous.toMutableList()
+        mutableRendezvousList -= rendezvous
+        madeRendezvous = mutableRendezvousList.toList()
+
+        rendezvousRepository.delete(rendezvous)
+    }
 
     fun isDeleted(): Boolean = deletedAt != null
 
