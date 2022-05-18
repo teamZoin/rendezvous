41002dc1970e9403ede27d26606ea756f2f50c7d
Kayoung <rkdkdudjd@gmail.com>
2022-05-18 12:19:30 +0900

    test: delete rendezvous

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,12 @@ repositories {
     mavenCentral()
 }
 
-subprojects {
+val springProjects = listOf(
+    project(":rendezvous-core"),
+    project(":rendezvous-rest"),
+)
+
+configure(springProjects) {
     apply(plugin = "java")
     apply(plugin = "idea")
     apply(plugin = "kotlin")
@@ -36,8 +41,27 @@ subprojects {
         mavenCentral()
     }
 
-    group = "com.zoin.rendezvous"
-    version = "0.0.1"
+    dependencies {
+        testImplementation("org.springframework.boot:spring-boot-starter-test") {
+            exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+        }
+        testImplementation("io.kotest.extensions:kotest-extensions-spring:_")
+        testImplementation("com.ninja-squad:springmockk:_")
+    }
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "idea")
+    apply(plugin = "kotlin")
+    apply(plugin = "kotlin-kapt")
+
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    repositories {
+        mavenCentral()
+    }
 
     dependencies {
         implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -51,8 +75,6 @@ subprojects {
         testImplementation("io.kotest:kotest-assertions-core:_")
         testImplementation("io.kotest:kotest-property:_")
         testImplementation("io.kotest:kotest-framework-datatest:_")
-        testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:_")
-        testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:_")
     }
 }
 
@@ -63,6 +85,6 @@ tasks.withType<KotlinCompile> {
     }
 }
 
-tasks.withType<Test>().configureEach {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
