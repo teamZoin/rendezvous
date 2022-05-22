2bf33cd40d62628430f5e10cd385570be1b3f789
Kayoung <rkdkdudjd@gmail.com>
2022-05-22 23:14:23 +0900

    feat: delete rendezvous usecase

---
+++ rendezvous-core/src/main/kotlin/com/zoin/rendezvous/domain/rendezvous/usecase/DeleteRendezvousUseCase.kt
@@ -0,0 +1,26 @@
+package com.zoin.rendezvous.domain.rendezvous.usecase
+
+import com.zoin.rendezvous.domain.rendezvous.repository.RendezvousRepository
+import com.zoin.rendezvous.domain.user.repository.UserRepository
+import javax.inject.Named
+
+@Named
+class DeleteRendezvousUseCase(
+    private val userRepository: UserRepository,
+    private val rendezvousRepository: RendezvousRepository,
+) {
+    data class Command(
+        val userId: Long,
+        val rendezvousId: Long,
+    )
+
+    fun execute(command: Command) {
+        val deleteAgent = userRepository.findById(command.userId)
+            .orElseThrow { IllegalArgumentException("User not found. user id: ${command.userId}") }
+        val rendezvous = rendezvousRepository.findByIdExcludeDeleted(command.rendezvousId)
+
+        if (rendezvous.creator != deleteAgent) throw IllegalAccessException("Only creator can delete reandezvous.")
+
+        deleteAgent.deleteRendezvous(rendezvousRepository, rendezvous)
+    }
+}
