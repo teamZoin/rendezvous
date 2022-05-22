758229871a98d39d6f5abd447189bf0711999738
Kayoung <rkdkdudjd@gmail.com>
2022-05-22 14:15:06 +0900

    feat: rendezvous 조회 API

--- rendezvous-rest/src/main/kotlin/com/zoin/rendezvous/api/RendezvousController.kt
+++ rendezvous-rest/src/main/kotlin/com/zoin/rendezvous/api/RendezvousController.kt
@@ -3,9 +3,12 @@ package com.zoin.rendezvous.api
 import com.zoin.rendezvous.api.`interface`.Response
 import com.zoin.rendezvous.api.`interface`.dto.CreateRendezvousReqDto
 import com.zoin.rendezvous.domain.rendezvous.usecase.CreateRendezvousUseCase
+import com.zoin.rendezvous.domain.rendezvous.usecase.ReadRendezvousUseCase
 import com.zoin.rendezvous.resolver.AuthTokenPayload
 import com.zoin.rendezvous.util.authToken.TokenPayload
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/rendezvous")
 class RendezvousController(
     private val createRendezvousUseCase: CreateRendezvousUseCase,
+    private val readRendezvousUseCase: ReadRendezvousUseCase,
 
 ) {
     @PostMapping("")
@@ -38,6 +42,24 @@ class RendezvousController(
         return Response(
             status = HttpStatus.OK.value(),
             message = "성공"
+        )
+    }
+
+    @GetMapping("/{id}")
+    fun getRendezvous(
+        @AuthTokenPayload payload: TokenPayload,
+        @PathVariable(value = "id") rendezvousId: Long,
+    ): Response<ReadRendezvousUseCase.RendezvousAndReader> {
+        val rendezvousAndReader = readRendezvousUseCase.execute(
+            ReadRendezvousUseCase.Query(
+                rendezvousId,
+                payload.userId
+            )
+        )
+        return Response(
+            status = HttpStatus.OK.value(),
+            message = "번개 조회 성공",
+            data = rendezvousAndReader
         )
     }
 }
