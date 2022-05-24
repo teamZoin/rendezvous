7c8c5f5b560dfb7802126a67c54dab45347f8ef8
Kayoung <rkdkdudjd@gmail.com>
2022-05-24 22:52:59 +0900

    feat: method for finding rendezvous page by cursor

--- rendezvous-core/src/main/kotlin/com/zoin/rendezvous/domain/rendezvous/repository/RendezvousCustomRepository.kt
+++ rendezvous-core/src/main/kotlin/com/zoin/rendezvous/domain/rendezvous/repository/RendezvousCustomRepository.kt
@@ -4,4 +4,6 @@ import com.zoin.rendezvous.domain.rendezvous.Rendezvous
 
 interface RendezvousCustomRepository {
     fun findByIdExcludeDeleted(id: Long): Rendezvous
+    fun findPageByCursorLimitSize(size: Long, cursorId: Long?): List<Rendezvous>
+    fun hasNextElement(endId: Long): Boolean
 }
