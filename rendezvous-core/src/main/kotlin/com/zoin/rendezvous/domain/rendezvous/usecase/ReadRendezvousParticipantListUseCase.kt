7371063479c8692f49838c21b9aea24ac39b879e
Kayoung <rkdkdudjd@gmail.com>
2022-05-27 01:10:01 +0900

    fix: exception message

--- rendezvous-core/src/main/kotlin/com/zoin/rendezvous/domain/rendezvous/usecase/ReadRendezvousParticipantListUseCase.kt
+++ rendezvous-core/src/main/kotlin/com/zoin/rendezvous/domain/rendezvous/usecase/ReadRendezvousParticipantListUseCase.kt
@@ -1,6 +1,5 @@
 package com.zoin.rendezvous.domain.rendezvous.usecase
 
-import com.zoin.rendezvous.domain.rendezvous.repository.RendezvousParticipantRepository
 import com.zoin.rendezvous.domain.rendezvous.repository.RendezvousRepository
 import com.zoin.rendezvous.domain.user.UserVO
 import com.zoin.rendezvous.domain.user.repository.UserRepository
@@ -10,17 +9,17 @@ import javax.inject.Named
 class ReadRendezvousParticipantListUseCase(
     private val rendezvousRepository: RendezvousRepository,
     private val userRepository: UserRepository,
-    private val rendezvousParticipantRepository: RendezvousParticipantRepository,
 ) {
     data class Query(
         val rendezvousId: Long,
         val readerId: Long,
     )
+
     fun execute(query: Query): List<UserVO> {
         val (rendezvousId, renderId) = query
         val rendezvous = rendezvousRepository.findByIdExcludeDeleted(rendezvousId)
         val reader = userRepository.findByIdExcludeDeleted(renderId)
-        if (rendezvous.creator != reader) throw IllegalAccessException("참여자는 번개 주최자만 볼 수 있어요.")
+        if (rendezvous.creator != reader) throw IllegalAccessException("번개 작성자가 아닙니다.")
         return rendezvous.participants.map {
             UserVO.of(it.participant)
         }
