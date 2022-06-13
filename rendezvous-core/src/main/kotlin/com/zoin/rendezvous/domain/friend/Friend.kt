fe9194c6229f16b5331a77ba1311c5ccab461b02
Kayoung <rkdkdudjd@gmail.com>
2022-06-13 17:22:42 +0900

    feat: 친구 도메인

---
+++ rendezvous-core/src/main/kotlin/com/zoin/rendezvous/domain/friend/Friend.kt
@@ -0,0 +1,48 @@
+package com.zoin.rendezvous.domain.friend
+
+import com.zoin.rendezvous.base.JpaBaseEntity
+import com.zoin.rendezvous.domain.user.User
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+
+@Entity
+@Table(name = "friend")
+class Friend(
+    @ManyToOne
+    val user: User,
+    @ManyToOne
+    val friend: User,
+    status: Status,
+) : JpaBaseEntity() {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0
+
+    var status: Status = status
+        private set
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Friend
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+}
+
+enum class Status {
+    FRIEND,
+    PENDING
+}
