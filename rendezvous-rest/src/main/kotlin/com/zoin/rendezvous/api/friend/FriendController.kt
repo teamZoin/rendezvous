7bd34db94cce8b14da59864b681308e3d92bd93b
Kayoung <rkdkdudjd@gmail.com>
2022-06-14 09:31:42 +0900

    feat: 친구 신청 수락 api

--- rendezvous-rest/src/main/kotlin/com/zoin/rendezvous/api/friend/FriendController.kt
+++ rendezvous-rest/src/main/kotlin/com/zoin/rendezvous/api/friend/FriendController.kt
@@ -1,11 +1,14 @@
 package com.zoin.rendezvous.api.friend
 
 import com.zoin.rendezvous.api.common.Response
+import com.zoin.rendezvous.api.friend.dto.AcceptFriendRequestDto
 import com.zoin.rendezvous.api.friend.dto.CreateFriendRequestDto
+import com.zoin.rendezvous.domain.friend.usecase.AcceptFriendRequestUseCase
 import com.zoin.rendezvous.domain.friend.usecase.CreateFriendRequestUseCase
 import com.zoin.rendezvous.resolver.AuthTokenPayload
 import com.zoin.rendezvous.util.authToken.TokenPayload
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/friend")
 class FriendController(
     private val createFriendRequestUseCase: CreateFriendRequestUseCase,
+    private val acceptFriendRequestUseCase: AcceptFriendRequestUseCase,
 ) {
 
     @PostMapping
@@ -29,6 +33,22 @@ class FriendController(
         )
         return Response(
             message = "친구 신청 성공"
+        )
+    }
+
+    @PutMapping
+    fun acceptFriendRequest(
+        @AuthTokenPayload payload: TokenPayload,
+        @RequestBody req: AcceptFriendRequestDto,
+    ): Response<Unit> {
+        acceptFriendRequestUseCase.execute(
+            AcceptFriendRequestUseCase.Command(
+                targetUserId = payload.userId,
+                requesterId = req.requesterId,
+            )
+        )
+        return Response(
+            message = "친구 신청 수락 성공"
         )
     }
 }
