d73484cbc26e0415c00beada334fd9216f96b8ee
Kayoung <rkdkdudjd@gmail.com>
2022-06-14 09:31:33 +0900

    feat: 친구 신청 수락 usecase

--- rendezvous-core/src/main/kotlin/com/zoin/rendezvous/domain/friend/Friend.kt
+++ rendezvous-core/src/main/kotlin/com/zoin/rendezvous/domain/friend/Friend.kt
@@ -29,6 +29,12 @@ class Friend(
     var status: Status = status
         private set
 
+    fun isFriend() = status == Status.FRIEND
+
+    fun acceptFriendRequest() {
+        status = Status.FRIEND
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
