b4f529f656e3bad4546c99cd06b8d127bba13805
Kayoung <rkdkdudjd@gmail.com>
2022-07-04 17:29:44 +0900

    feat: create bean conditional on property

--- rendezvous-rest/src/main/kotlin/com/zoin/rendezvous/config/JwtUtilConfig.kt
+++ rendezvous-rest/src/main/kotlin/com/zoin/rendezvous/config/JwtUtilConfig.kt
@@ -1,4 +1,21 @@
 package com.zoin.rendezvous.config
 
-class JwtUtilConfig {
+import com.zoin.rendezvous.UtilEnvHolder
+import com.zoin.rendezvous.jwt.JwtUtil
+import com.zoin.rendezvous.jwt.MockJwtUtil
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class JwtUtilConfig(
+    private val utilEnvHolder: UtilEnvHolder,
+) {
+    @Bean
+    @ConditionalOnProperty(name = ["jwt.mock"], havingValue = "false")
+    fun jwtUtil() = JwtUtil(utilEnvHolder)
+
+    @Bean
+    @ConditionalOnProperty(name = ["jwt.mock"], havingValue = "true")
+    fun mockJwtUtil() = MockJwtUtil(utilEnvHolder)
 }
