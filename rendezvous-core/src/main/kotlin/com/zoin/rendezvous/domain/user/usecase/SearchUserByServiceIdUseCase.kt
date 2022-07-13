6ff3976380b6ad6079f79bce8a446b0a1beaee07
Kayoung <rkdkdudjd@gmail.com>
2022-07-14 03:11:25 +0900

    feat: 친구 관계도 함께 조회하기

--- rendezvous-core/src/main/kotlin/com/zoin/rendezvous/domain/user/usecase/SearchUserByServiceIdUseCase.kt
+++ rendezvous-core/src/main/kotlin/com/zoin/rendezvous/domain/user/usecase/SearchUserByServiceIdUseCase.kt
@@ -1,5 +1,7 @@
 package com.zoin.rendezvous.domain.user.usecase
 
+import com.zoin.rendezvous.domain.friend.UserRelationship
+import com.zoin.rendezvous.domain.friend.repository.FriendRepository
 import com.zoin.rendezvous.domain.user.User
 import com.zoin.rendezvous.domain.user.repository.UserRepository
 import javax.inject.Named
@@ -7,11 +9,28 @@ import javax.inject.Named
 @Named
 class SearchUserByServiceIdUseCase(
     private val userRepository: UserRepository,
+    private val friendRepository: FriendRepository,
 ) {
     data class Query(
+        val userId: Long,
         val searchIdInput: String,
     )
-    fun execute(query: Query): List<User> {
-        return userRepository.findByServiceIdContaining(query.searchIdInput)
+
+    fun execute(query: Query): List<Pair<User, UserRelationship>> {
+        val actor = userRepository.findByIdExcludeDeleted(query.userId)
+        return userRepository.findByServiceIdContaining(query.searchIdInput).map { searchedUser ->
+            val friendRequest =
+                friendRepository.findByUserAndFriend(actor, searchedUser)
+                    ?: return@map searchedUser to UserRelationship.NOT_FRIEND
+            if (friendRequest.isFriend()) {
+                searchedUser to UserRelationship.FRIEND
+            } else {
+                if (friendRequest.user == actor) {
+                    searchedUser to UserRelationship.WAITING
+                } else {
+                    searchedUser to UserRelationship.HOLDING
+                }
+            }
+        }
     }
 }
