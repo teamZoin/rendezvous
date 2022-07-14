5f005053b5ed45e14245907f19353f6214c3df49
Kayoung <rkdkdudjd@gmail.com>
2022-07-14 15:02:24 +0900

    feat: 로그인 usecase 반환값 수정

--- rendezvous-core/src/main/kotlin/com/zoin/rendezvous/domain/user/usecase/LoginUseCase.kt
+++ rendezvous-core/src/main/kotlin/com/zoin/rendezvous/domain/user/usecase/LoginUseCase.kt
@@ -1,5 +1,6 @@
 package com.zoin.rendezvous.domain.user.usecase
 
+import com.zoin.rendezvous.domain.user.User
 import com.zoin.rendezvous.domain.user.repository.UserRepository
 import com.zoin.rendezvous.util.PasswordEncoder
 import javax.inject.Named
@@ -11,10 +12,10 @@ class LoginUseCase(
 ) {
     data class Query(val email: String, val password: String)
 
-    fun execute(query: Query): Long {
+    fun execute(query: Query): User {
         val user = userRepository.findByEmail(query.email) ?: throw IllegalArgumentException("존재하지 않는 이메일입니다.")
         val hashedPassword = passwordEncoder.encode(user.salt, query.password)
         if (!user.hashedPassword.contentEquals(hashedPassword)) throw IllegalArgumentException("비밀번호가 일치하지 않습니다.")
-        return user.id!!
+        return user
     }
 }
